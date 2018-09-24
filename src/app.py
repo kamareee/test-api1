85c3bbbc01864648693ae0ab2dab27a2a282516f
kamar34 <mohammadkamar.uddin@tmrnd.com.my>
2018-09-24 15:22:57 +0800

    added run.py file and __init__.py file

--- src/app.py
+++ src/app.py
@@ -5,13 +5,10 @@ from flask_restful import Api
 from resources.entry_api import HelloWorld
 
 app = Flask(__name__)
-
-app.config['DEBUG'] = True
-
+app.config.from_object('src.config')
 app.secret_key = 'mysecret'
+
 api = Api(app)
 
 api.add_resource(HelloWorld, '/expert.do')
 
-if __name__ == '__main__':
-    app.run(port=5000)
---
+++ src/run.py
@@ -0,0 +1,5 @@
+
+from src.app import app
+
+app.run(debug=app.config['DEBUG'], port=5000)
+
